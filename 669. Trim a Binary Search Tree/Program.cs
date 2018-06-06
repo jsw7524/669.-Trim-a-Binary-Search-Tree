9b89f0855c6daa0760b812260cec177a8b04bb40
jsw7524 <jsw7524@gmail.com>
2018-06-06 22:22:54 +0800

    AC

--- 669. Trim a Binary Search Tree/Program.cs
+++ 669. Trim a Binary Search Tree/Program.cs
@@ -6,6 +6,63 @@ using System.Threading.Tasks;
 
 namespace _669.Trim_a_Binary_Search_Tree
 {
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    public class Solution
+    {
+        public TreeNode TrimBST(TreeNode root, int L, int R)
+        {
+            root = TrimLower(root, L);
+            root = TrimUpper(root, R);
+            return root;
+        }
+        public TreeNode TrimLower(TreeNode root, int L)
+        {
+            if (null == root)
+            {
+                return null;
+            }
+            else if (L==root.val)
+            {
+                root.left = null;
+            }
+            else if (L < root.val)
+            {
+                root.left=TrimLower(root.left,L);
+            }
+            else if (L > root.val)
+            {
+                root = TrimLower(root.right, L);
+            }
+            return root;
+        }
+        public TreeNode TrimUpper(TreeNode root, int R)
+        {
+            if (null == root)
+            {
+                return null;
+            }
+            else if (R == root.val)
+            {
+                root.right = null;
+            }
+            else if (R < root.val)
+            {
+                root = TrimUpper(root.left, R);
+            }
+            else if (R > root.val)
+            {
+                root.right = TrimUpper(root.right, R);
+            }
+            return root;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using _669.Trim_a_Binary_Search_Tree;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TreeNode root = new TreeNode(1) { left=new TreeNode(0),right = new TreeNode(2) };
+            Solution sln = new Solution();
+            var result = sln.TrimBST(root, 1, 2);
+            TreeNode ans = new TreeNode(1) { left = null, right = new TreeNode(2) };
+            Assert.AreEqual(JsonConvert.SerializeObject(result), JsonConvert.SerializeObject(ans));
+        }
+    }
+}
